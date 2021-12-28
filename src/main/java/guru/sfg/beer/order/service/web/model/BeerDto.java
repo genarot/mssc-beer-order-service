4f1309d3c1a60f5838212346b3ec692c93bf104f
Tinoco, Genaro <gtinoco@agssa.net>
2021-12-28 14:46:30 -0600

    Adding Beer information to Orders

---
+++ src/main/java/guru/sfg/beer/order/service/web/model/BeerDto.java
@@ -0,0 +1,42 @@
+/* Copyright (C) PublicRelay, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * The work belongs to the author's employer under work made for hire principles.
+ */
+package guru.sfg.beer.order.service.web.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+/**
+ * @author gtinoco
+ * @since 12/28/21
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID id = null;
+    private Integer version = null;
+
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    private OffsetDateTime createdDate = null;
+
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    private OffsetDateTime lastModifiedDate = null;
+    private String beerName;
+    private String beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+
+    @JsonFormat(shape= JsonFormat.Shape.STRING)
+    private BigDecimal price;
+}
--- src/main/java/guru/sfg/beer/order/service/web/model/BeerOrderLineDto.java
+++ src/main/java/guru/sfg/beer/order/service/web/model/BeerOrderLineDto.java
@@ -22,6 +22,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -30,18 +31,21 @@ import java.util.UUID;
 @EqualsAndHashCode(callSuper = true)
 public class BeerOrderLineDto extends BaseItem {
 
+    private String upc;
+    private String beerName;
+    private String beerStyle;
+    private BigDecimal price;
+    private UUID beerId;
+    private Integer orderQuantity = 0;
     @Builder
     public BeerOrderLineDto(UUID id, Integer version, OffsetDateTime createdDate, OffsetDateTime lastModifiedDate,
-                            String upc, String beerName, UUID beerId, Integer orderQuantity) {
+                            String upc, String beerName, String beerStyle, BigDecimal price, UUID beerId, Integer orderQuantity) {
         super(id, version, createdDate, lastModifiedDate);
         this.upc = upc;
         this.beerName = beerName;
+        this.beerStyle = beerStyle;
+        this.price = price;
         this.beerId = beerId;
         this.orderQuantity = orderQuantity;
     }
-
-    private String upc;
-    private String beerName;
-    private UUID beerId;
-    private Integer orderQuantity = 0;
 }
